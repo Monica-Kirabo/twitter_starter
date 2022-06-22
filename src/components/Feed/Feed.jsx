85a556bd52d55957472a5fa0e86b39d28f5d57a5
monica kirabo <kirabom@duq.edu>
2022-06-22 16:07:41 -0700

    commit here

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -3,14 +3,14 @@ import Tweet from "../Tweet/Tweet";
 import TweetBox from "../TweetBox/TweetBox";
 import "./Feed.css";
 
-export default function Feed(props) {
+export default function Feed({ tweets, setTweets, userProfile }) {
   return (
     <div className="col feed">
       {/* UPDATE TWEET BOX PROPS HERE */}
       <TweetBox
-        setTweets={props.setTweets}
-        tweets={props.tweets}
-        userProfile={props.userProfile}
+        setTweets={setTweets}
+        tweets={tweets}
+        userProfile={userProfile}
       />
 
       <div className="see-new-tweets beet">
@@ -19,7 +19,14 @@ export default function Feed(props) {
         </p>
       </div>
 
-      <div className="twitter-feed">{/* ADD CODE HERE */}</div>
+      <div className="twitter-feed">
+        {
+          /* ADD CODE HERE */
+          tweets.map((tweet, idx) => {
+            return <Tweet key={idx} tweet={tweet} />;
+          })
+        }
+      </div>
     </div>
   );
 }
